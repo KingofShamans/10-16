2baba119e14da01fa60bcaecabf38a8b61b8a1ab
KingofShamans <80450451+KingofShamans@users.noreply.github.com>
2021-09-04 09:45:26 +0300

    Add files via upload

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите десятичное число");
+            int a = Convert.ToInt32(Console.ReadLine());
+            string b = Convert.ToString(a, 16);
+            Console.WriteLine(b);
+        }
+    }
+}
